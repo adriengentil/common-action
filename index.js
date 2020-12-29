d59a827565f841b9d217d8dd0ac0fceae4aaad43
Adrien Gentil <adrien.gentil@adevinta.com>
2020-12-29 16:43:47 +0100

    docker login

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const dockerhubUsername = 'adriengentil'
+  const dockerhubPassword = core.getInput('dockerhub-password');
+  console.log(`Log in to dockerhub with ${dockerhubUsername} / ${dockerhubPassword}`);
+  exec.exec('docker', ['login', '--username', dockerhubUsername, '--password', dockerhubPassword]);
 } catch (error) {
   core.setFailed(error.message);
 }
