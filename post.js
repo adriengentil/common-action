ba1f3be113c9f5ffc7e3614c780bfd8e0df17d55
Adrien Gentil <adrien.gentil@adevinta.com>
2020-12-30 12:34:05 +0100

    catch

--- post.js
+++ post.js
@@ -6,13 +6,11 @@ try {
   const image = 'adriengentil/hello-actions'
   exec.exec('docker', ['build', '-t', image, '.'])
            .then((res) => {
-                   if (res === 0) {
                    exec.exec('docker', ['push', image])
-                   }
-                   else {
+                 })
+           .catch(() => {
                      core.setFailed(`Action failed cannot build docker image`);
-                   }
-                 });
+                  });
 } catch (error) {
   core.setFailed(error.message);
 }
